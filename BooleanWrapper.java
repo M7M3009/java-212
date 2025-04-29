25bae8ed4e09d77603f0e5c1193faf496b3c3c87
M7M3009 <malhwydy2@gmail.com>
2025-04-29 16:44:24 +0300

    Add files via upload

---
+++ BooleanWrapper.java
@@ -0,0 +1,20 @@
+package project;
+
+public class BooleanWrapper {
+	private boolean wrapper;
+
+	public BooleanWrapper(boolean wrapper) {
+		
+		this.wrapper = wrapper;
+	}
+
+	public boolean getWrapper() {
+		return wrapper;
+	}
+
+	public void setWrapper(boolean wrapper) {
+		this.wrapper = wrapper;
+	}
+	
+	
+}
