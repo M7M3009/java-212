b2e8f283dc33ab22db8b9409e63fc875697f4a56
n-j-m-n <abbody619@hotmail.com>
2025-05-03 15:38:08 +0300

    Update PhotoManager.java

--- PhotoManager.java
+++ PhotoManager.java
@@ -10,8 +10,57 @@ public class PhotoManager {
 		this.Mph = new linkedlist<Photo>();
 	}
 
-	public linkedlist<Photo> getPhotos() {
+	public LinkedList<photo> getPho() {
+
+		LinkedList<photo> t = manager.getPhoto();
+		t.findFirst();
+
+		while (!t.empty() && t.retrieve() != null) {
+			String arr[] = splitcond(condition);//maybe here
+			photo g = t.retrieve();
+			LinkedList<String> tags = g.getTags();
+			tags.findFirst();
+			while (!tags.empty() && tags.retrieve() != null) {
+				for (int i = 0; i < arr.length; i++) {
+					compare++;
+					if (!tags.retrieve().equals(arr[i])) {
+						tags.findNext();
+						if (tags.retrieve()==null)
+							break;
+						continue;
+					} else if (tags.retrieve().equals(arr[i])) {
+						arr[i] = null;
+						tags.findNext();
+						if (tags.retrieve()==null)
+							break;
+						continue;
+					}
+					
+				
+				}
+				boolean allMatched = true;
+				for (String s : arr) {
+					if (s != null) {
+						allMatched = false;
+						break;
+					}
+				}
+				if (allMatched) {
+					pho.insert(g);
+					//tags.findNext();//
+					
+					
+				}
+
+			}
+			t.findNext();
+			if (t.retrieve()==null) {
+				break;
+				
+			}
+		}
 		
+		return pho;
 	}
 
 	public void addPhoto(Photo p) {
